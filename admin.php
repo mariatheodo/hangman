40f5cfb7f28d9b69649c8d4a9efc3eaa48121125
mariatheodo <maria.theodo@gmail.com>
2015-12-27 23:52:24 +0200

    final bar footer

--- admin.php
+++ admin.php
@@ -1,47 +1,11 @@
-<!DOCTYPE html>
-
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Hangman Game (Κρεμάλα)</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href='https://fonts.googleapis.com/css?family=Lato:100,300' rel='stylesheet' type='text/css'>
-	<script src="js/bootstrap.js"></script>
-	
-	<script src="css/config.json"></script>
-	<style id="style-1-cropbar-clipper">/* Copyright 2014 Evernote Corporation. All rights reserved. */
-		.en-markup-crop-options {
-		top: 18px !important;
-		left: 50% !important;
-		margin-left: -100px !important;
-		width: 200px !important;
-		border: 2px rgba(255,255,255,.38) solid !important;
-		border-radius: 4px !important;
-		}
-
-		.en-markup-crop-options div div:first-of-type {
-		margin-left: 0px !important;
-		}
-	</style>
-
-
-
-</head>
-
-<body>
 	<?php 
 	if (isset($_POST['name']) && $_POST['name'] == '123456') {
 		header("Location: admin_pages.php");
 	}
-	else { ?>
-	<div class="container">
-		<div class="row">
-			<div class="page-header">
-				<h1>ΚΡΕΜΑΛΑ</h1>
-			</div>
-		</div>
-		<div class="jumbotron main-page">
+	else { 
+		include_once 'header.html';
+	?>
+
 			<h3>Καλωσήρθες διαχειριστή</h3>
 			<h3>Δώσε το συνθηματικό σου</h3>
 			<h3></h3>
